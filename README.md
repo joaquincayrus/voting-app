97495c63fc96dd292fe4b94737746219f9ea4833
Joaquin Cayrus <jcayrus@gmail.com>
2020-03-02 16:54:32 -0300

    Connection to sqlite and vs colors

--- README.md
+++ README.md
@@ -14,6 +14,13 @@ this install angular cli 9
 
 ### NodeJS App
 
+#### SQLite
+
+Installation: https://www.sqlitetutorial.net/download-install-sqlite/
+
+or download directly from: https://www.sqlite.org/2020/sqlite-tools-win32-x86-3310100.zip
+
+follow instructions: -for Windows install in C:\sqlite
 
 
 ### Angular App
--- voting-app-backend/index.js
+++ voting-app-backend/index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const sqlite3 = require('sqlite3');
 
 const app = express();
 const port = 3000;
 
-// Where we will keep books
-let elections = [];
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+db.serialize(() => {
+    // Queries scheduled here will be serialized.
+    //TODO: Create table crea una tabla nueva y datos de prueba hay que cambiar los atributos de la tabla para que tome todos los necesarios
+    db.run('CREATE TABLE election(title text)')
+      .run(`INSERT INTO election(title)
+            VALUES('Election 1'),
+                  ('Election 2'),
+                  ('Election 3')`)
+      .each(`SELECT title FROM election`, (err, row) => {
+        if (err){
+          throw err;
+        }
+        console.log(row.title);
+      });
+});
 
 app.use(cors());
-
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,12 +36,33 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 })
+app.get('/election', (req, res) => {
+    let sql = `SELECT *
+            FROM election`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            console.log(row.title);
+        });
+        res.send(rows);
+    });        
+})
 app.post('/election', (req, res) => {
     const election = req.body;
-    // Output the book to the console for debugging
+    // Output the election to the console for debugging
     console.log(election);
-    books.push(election);
+    //TODO: Agregar la nueva elección a la base de datos en memoria
     res.send('Election is added to the database');
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`)).on("close", () => {
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+});
